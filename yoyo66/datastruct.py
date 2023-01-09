1057357de57b859ba28af099a9b58c64a8d1de75
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2023-01-09 13:43:22 -0500

    update layer and multi-layer image

--- yoyo66/datastruct.py
+++ yoyo66/datastruct.py
@@ -20,9 +20,9 @@ Dimension class has two main fields: width and height.
 Dimension = namedtuple('Dimension', ['width', 'height'])
 
 @dataclass
-class phmLayer:
+class Layer:
     """
-    phmLayer is a base class for layers inside a multi-layer image file.
+    Layer is a base class for mask layers inside a multi-layer image file.
     """
     
     # The index of the layer
@@ -33,6 +33,8 @@ class phmLayer:
     opacity : float = field(default=1.0)
     # visibility (bool) determine whether the layer is hidden (False) or not (True). Default True
     visibility : bool = field(default=True)
+    # Class identifier
+    class_id : int = field(default=1)
     # image (PIL.Image) the imagery data of the layer. Default None
     image : Image = field(default=None)
     # x (int) the x position of the layer. Normally it should be always zero but it can be non-zero in case the layer is smaller than the image size.
@@ -40,16 +42,21 @@ class phmLayer:
     # y (int) the y position of the layer. Normally it should be always zero but it can be non-zero in case the layer is smaller than the image size.
     y : int = field(default=0)
 
-    def is_valid(self):
+    def is_valid(self) -> bool:
+        """Check if the layer is valid. The image is valid if the image field is initialized!
+
+        Returns:
+            bool: True if valid
+        """
         return self.image is not None
 
 class phmImage(ABC):
     
     def __init__(self,
         filepath : str,
-        dimension : Dimension,
         properties : Dict,
-        layers : List[phmLayer],
+        original_image : Image,
+        layers : List[Layer],
         title : str = None
     ):
         super().__init__()
@@ -59,8 +66,6 @@ class phmImage(ABC):
         self.filename = os.path.basename(self.filepath)
         # Image title
         self.title = Path(self.filepath).stem if title is None else title
-        # Image dimensions
-        self.dimension = dimension
         # Properties
         self.properties = {
             'filepath' : self.filepath,
@@ -71,7 +76,7 @@ class phmImage(ABC):
         # Layers
         self.layers = layers
 
-    def __getitem__(self, key) -> Union[Dict, phmLayer] :
+    def __getitem__(self, key) -> Union[Dict, Layer] :
         res = None
         if key == 'properties':
             res = self.properties
@@ -91,34 +96,41 @@ class phmImage(ABC):
     def set_property(self, prop, value) -> None:
         self.properties[prop] = value
     
-    def get_layer(self, layer_name : str) -> phmLayer:
+    def get_layer(self, layer_name : str) -> Layer:
         layer = None
         for l in self.layers:
             if layer_name == l.name:
                 layer = l
+                break
         
         if layer is None:
             raise KeyError('%s layer does not exist!' % layer_name)
         return layer
     
-    def get_layer_by_index(self, index : int) -> phmLayer:
-        if index >= len(self.layers):
+    def get_layer_by_index(self, index : int) -> Layer:
+        if index >= len(self.layers) or index < 0:
             raise IndexError('Index %d does not exist' % index)
         return self.layers[index]
     
-    def get_all_layers(self) -> Tuple[phmLayer]:
-        return tuple(self.layers)
-
-    def get_layer_names(self) -> Tuple[str]:
-        return tuple(map(lambda x : x.name, self.layers))
+    def get_original_image(self) -> Image:
+        return self.original_image
     
-    def get_original_image(self):
-        return self.layers[__original_key__]
+    @property
+    def dimension(self):
+        return self.original_image.size if self.original_image is not None else None
+    
+    @property
+    def mask_layers(self) -> Tuple[Layer]:
+        return tuple(self.layers)
+    
+    @property
+    def layers_name(self) -> Tuple[str]:
+        return tuple(map(lambda x : x.name, self.layers))
     
     @property
     def layer_names(self) -> Tuple[str]:
         return self.get_layer_names()
-    
-    @property
-    def original_image(self) -> Image:
-        return self.get_original_image()
+
+    def __iter__(self):
+        orig = Layer(id = 0, name = 'original', image = self.original_image)
+        return iter(tuple([orig]) + self.mask_layers)
