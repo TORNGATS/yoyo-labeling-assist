e32a04bc5a89e0c88a580f288c5d6da7856e9693
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2023-01-12 00:09:02 -0500

    update multi-layer image structure

--- yoyo66/datastruct.py
+++ yoyo66/datastruct.py
@@ -10,8 +10,9 @@ from pathlib import Path
 from typing import Dict, List, Tuple, Union
 
 from PIL import Image
+import numpy as np
 
-__original_key__ = 'original'
+ORIGINAL_LAYER_KEY = 'original'
 
 """
 Dimension is a entity class used to keep the dimension al values (width and height)
@@ -24,9 +25,7 @@ class Layer:
     """
     Layer is a base class for mask layers inside a multi-layer image file.
     """
-    
-    # The index of the layer
-    id : int
+
     # name (str) the name of layer 
     name : str
     # opacity (float) the opacity of the layer. Default 1.0
@@ -36,26 +35,43 @@ class Layer:
     # Class identifier
     class_id : int = field(default=1)
     # image (PIL.Image) the imagery data of the layer. Default None
-    image : Image = field(default=None)
+    image : np.ndarray = field(default=None)
     # x (int) the x position of the layer. Normally it should be always zero but it can be non-zero in case the layer is smaller than the image size.
     x : int = field(default=0)
     # y (int) the y position of the layer. Normally it should be always zero but it can be non-zero in case the layer is smaller than the image size.
     y : int = field(default=0)
 
     def is_valid(self) -> bool:
-        """Check if the layer is valid. The image is valid if the image field is initialized!
+        """ Check if the layer is valid. The image is valid if the image field is initialized!
 
         Returns:
             bool: True if valid
         """
         return self.image is not None
 
-class phmImage(ABC):
+    @property
+    def dimension(self) -> Tuple[int, int]:
+        """ Dimension of the layer
+
+        Returns:
+            Tuple[int, int]: The size of the layer (width, height)
+        """
+        return self.image.size
+
+    @property
+    def classmap(self):
+        return self.image * self.class_id
+
+
+class phmImage:
+    """ 
+    It is the base class for the multi-layer image.
+    """
     
     def __init__(self,
         filepath : str,
         properties : Dict,
-        original_image : Image,
+        orig_image : Image,
         layers : List[Layer],
         title : str = None
     ):
@@ -75,18 +91,12 @@ class phmImage(ABC):
         }
         # Layers
         self.layers = layers
-
-    def __getitem__(self, key) -> Union[Dict, Layer] :
-        res = None
-        if key == 'properties':
-            res = self.properties
-        elif isinstance(key, str):
-            res = self.get_layer(key)
-        elif isinstance(key, numbers.Number):
-            res = self.get_layer_by_index(key)
-        else:
-            raise ValueError('Key %s is invalid' % key)
-        return res
+        # Original Layer
+        self.orig_layer = Layer(
+            name = ORIGINAL_LAYER_KEY,
+            image = orig_image,
+            x = 0, y = 0
+        )
    
     def get_property(self, prop):
         if not prop in self.properties:
@@ -97,40 +107,54 @@ class phmImage(ABC):
         self.properties[prop] = value
     
     def get_layer(self, layer_name : str) -> Layer:
-        layer = None
-        for l in self.layers:
-            if layer_name == l.name:
-                layer = l
-                break
-        
-        if layer is None:
-            raise KeyError('%s layer does not exist!' % layer_name)
-        return layer
+        resly = tuple(filter(lambda l : l.name == layer_name, self.layers))
+        if len(resly) == 0:
+             raise KeyError('%s layer does not exist!' % layer_name)
+
+        return resly[0]
     
     def get_layer_by_index(self, index : int) -> Layer:
         if index >= len(self.layers) or index < 0:
             raise IndexError('Index %d does not exist' % index)
+        
         return self.layers[index]
     
-    def get_original_image(self) -> Image:
-        return self.original_image
-    
+    def __getitem__(self, key) -> Union[Dict, Layer] :
+        res = None
+        if key == 'properties':
+            res = self.properties
+        elif key == ORIGINAL_LAYER_KEY:
+            res = self.orig_layer
+        elif isinstance(key, str):
+            res = self.get_layer(key)
+        elif isinstance(key, numbers.Number):
+            res = self.get_layer_by_index(key)
+        else:
+            raise ValueError('Key %s is invalid' % key)
+        return res
+
     @property
-    def dimension(self):
-        return self.original_image.size if self.original_image is not None else None
+    def original_layer(self) -> Layer:
+        return self.orig_layer
     
     @property
     def mask_layers(self) -> Tuple[Layer]:
         return tuple(self.layers)
-    
-    @property
-    def layers_name(self) -> Tuple[str]:
-        return tuple(map(lambda x : x.name, self.layers))
-    
+
     @property
     def layer_names(self) -> Tuple[str]:
         return self.get_layer_names()
 
+    @property
+    def dimension(self):
+        return self.orig_layer.dimension
+
+    @property
+    def count_layers(self):
+        return len(self.layers) + 1
+
     def __iter__(self):
-        orig = Layer(id = 0, name = 'original', image = self.original_image)
-        return iter(tuple([orig]) + self.mask_layers)
+        return iter(tuple([self.orig_layer]) + self.mask_layers)
+    
+    def __str__(self) -> str:
+        return f'{self.title} (Layers : {self.count_layers}) [Dimension : {self.dimension}]'
