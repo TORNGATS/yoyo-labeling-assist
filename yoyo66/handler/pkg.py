94ac1378cba8cfe4da33e3451c95e12f5f010ec4
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2023-01-13 11:50:51 -0500

    update

--- yoyo66/handler/pkg.py
+++ yoyo66/handler/pkg.py
@@ -1,4 +1,6 @@
 
+import zipfile
+
 from typing import Dict, List, Union
 from pyora import Project, TYPE_LAYER
 
@@ -7,6 +9,13 @@ from yoyo66.datastruct import phmImage, Layer, ORIGINAL_LAYER_KEY
 
 @mmfile_handler('pkg')
 class PKGFileHandler(BaseFileHandler): # Parham, Keven, and Gabriel (PKG)
+
     def __init__(self, categories: Union[Dict[str, int], List[str]]) -> None:
+        super().__init__(categories)
+        
+    def load(self, filepath: str) -> phmImage:
         pass
+        
+    def save(self, img: phmImage, file_path: str):
+        # save properties
         
