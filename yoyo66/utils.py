18d4e31050b72cd64cab17c6b7743d5fec75fa59
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2023-01-22 23:37:42 -0500

    Documenting utils

--- yoyo66/utils.py
+++ yoyo66/utils.py
@@ -19,6 +19,16 @@ class ConvertHandler:
         self.dest_handler = dest_handler
     
     def convert_file(self, source_file : str, dest_file : str) -> None:
+        """Convert a file `source_file` to a destination format
+
+        Args:
+            source_file (str): source file
+            dest_file (str): destination file
+
+        Raises:
+            ValueError: if source file is invalid
+            ValueError: if the multi-layer image is failed to load
+        """
         if not self.source_handler.is_valid(source_file):
             raise ValueError('file %s is not valid' % source_file)
         # Loading the multi-layer imagery data from the source file
@@ -27,7 +37,23 @@ class ConvertHandler:
             raise ValueError('The coversion process is failed for file %s' % source_file)
         self.dest_handler(dest_file, img)
         
-    def convert_dir(self, source_dir : str, dest_dir : str, lazy : bool = True) -> bool:
+    def convert_dir(self, source_dir : str, dest_dir : str, lazy : bool = True) -> Any:
+        """Convert all files inside a directory to a destination formation
+
+        Args:
+            source_dir (str): source directory (search string). Read ``glob.glob`` for more information.
+            dest_dir (str): destination directory
+            lazy (bool, optional): Determine if it loads all multi-layer images or one by one. Defaults to True.
+
+        Raises:
+            ValueError: if source or destination directory are invalid
+
+        Returns:
+            Any: a list containing the result of conversion
+
+        Yields:
+            Iterator[Any]: an iterator if `lazy` is True. Presenting the lazy version of conversion process
+        """
         if not os.path.isdir(source_dir) or not os.path.isdir(dest_dir):
             raise ValueError('given paths must be directories')
         
@@ -48,6 +74,19 @@ class ConvertHandler:
         return citer if lazy else list(citer)
             
     def __call__(self, source_file : str, dest_file : str, lazy : bool = False) -> Any:
+        """Conversion process. It automatically decides if it would be a directory or file conversion.
+
+        Args:
+            source_file (str): source path (file or directory (search string))
+            dest_file (str): destination directory
+            lazy (bool, optional): lazy process setting. Defaults to False.
+
+        Raises:
+            ValueError: if source path is invalid.
+
+        Returns:
+            Any: the result of conversion
+        """
         if os.path.isdir(source_file):
             return self.convert_dir(source_file, dest_file, lazy)
         elif os.path.isfile(source_file):
@@ -66,11 +105,11 @@ def build_converter(
     if src_handler is given, src_fextension will be ignored. Same case for dest_handler and dest_fextension/
 
     Args:
-        categories (Union[Dict[str, int], List[str]]): _description_
-        src_handler (str, optional): _description_. Defaults to None.
-        dest_handler (str, optional): _description_. Defaults to None.
-        src_fextension (str, optional): _description_. Defaults to None.
-        dest_fextension (str, optional): _description_. Defaults to None.
+        categories (Union[Dict[str, int], List[str]]): the categories containing the class name and associated class id
+        src_handler (str, optional): the file handler for source files. Defaults to None.
+        dest_handler (str, optional): the file handler for destination file conversion. Defaults to None.
+        src_fextension (str, optional): the file extension of source files. Defaults to None.
+        dest_fextension (str, optional): the file extension of destination files. Defaults to None.
     """
     
     src_obj = None
@@ -95,6 +134,13 @@ def build_converter(
     )
 
 def convert_file__(src_file : str, dest_file : str, categories : Union[Dict[str, int], List[str]]):
+    """Wrapping function for converting files.
+
+    Args:
+        src_file (str): source file path
+        dest_file (str): destination file path
+        categories (Union[Dict[str, int], List[str]]): List of categories
+    """
     src_ext = Path(src_file).suffix.replace('.', '')
     dest_ext = Path(dest_file).suffix.replace('.', '')
     
