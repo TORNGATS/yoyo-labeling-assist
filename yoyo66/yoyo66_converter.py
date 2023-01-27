9f50dbec8246c7a58fabae646f93f0b01a7351fb
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2023-01-27 01:59:11 -0500

    code update

--- yoyo66/yoyo66_converter.py
+++ yoyo66/yoyo66_converter.py
@@ -2,11 +2,12 @@
 import os
 import sys
 import argparse
-import json
 import glob
+import multiprocessing as mp
 
 from pathlib import Path
 from progress.bar import Bar
+from functools import partial
 
 sys.path.append(os.getcwd())
 sys.path.append(__file__)
@@ -21,6 +22,17 @@ from yoyo66.handler import (
 )
 from yoyo66.utils import convert_file__
 
+create_out_filepath = lambda fin, fout, type : os.path.join(fout, f'{Path(os.path.basename(fin)).stem}.{get_file_extensions(type)[0]}')
+
+def convert_process(file, outfile, type, override, classnames):
+    ofile = create_out_filepath(file, outfile, type)
+    try:
+        if not (not override and os.path.isfile(ofile)):
+            convert_file__(file, ofile, classnames)
+            return True, file
+    except Exception:
+        return False, file
+
 def main__():
     parser = argparse.ArgumentParser(
         prog = 'YoYo-66 Converter',
@@ -34,9 +46,9 @@ def main__():
     parser.add_argument('-m', '--mode', default = 'file', choices = ['file', 'directory'], help = 'Determine the mode of given file/directory.')
     parser.add_argument('-t', '--type', nargs = '?', choices = list_handler_names(), help = 'Determine the targeted file type. The file type is only used when the output is a directory')
     parser.add_argument('-c', '--classnames', type = str, nargs='*', help = 'Specify the list of class labels.')
-    parser.add_argument('-s', '--silent', action='store_true', help = 'Silent mode supress all raise exception.')
     parser.add_argument('--override', action='store_false', help = 'Override mode prevent conversion when the output file has already exist if not set.')
-    
+    parser.add_argument('-p', '--proc', type = int, default = 3, help = 'Number of process')
+
     args = parser.parse_args()
     
     if args.mode is None:
@@ -45,8 +57,6 @@ def main__():
     if args.input is None:
         print("input filepath must be specified!")
         return -1
-    
-    create_out_filepath = lambda fin, fout, type : os.path.join(fout, f'{Path(os.path.basename(fin)).stem}.{get_file_extensions(type)[0]}')
     
     files = []
     infile = args.input
@@ -68,22 +78,35 @@ def main__():
             return -1
 
         files = glob.glob(infile)
+        outfile = args.output
         
-        with Bar(' Converting', max=len(files), suffix='%(percent)d%%') as bar:
-            for fin in files:
-                outfile = args.output
-                bar.message = fin
-                outfile = create_out_filepath(fin, outfile, args.type)
-                try:
-                    if not (not args.override and os.path.isfile(outfile)):
-                        convert_file__(fin, outfile, args.classnames)
-                        print(' Successful')
-                except Exception as ex:
-                    if not args.silent:
-                        raise ex
-                    print(' Failed')
-                finally:
+        with mp.Pool(processes = args.proc) as pool:
+            with Bar(' Converting', max=len(files), suffix='%(percent)d%%') as bar:
+                func = partial(convert_process, 
+                    outfile = args.output,
+                    type = args.type,
+                    override = args.override,
+                    classnames = args.classnames
+                )
+                for res in pool.map(func, files):
+                    bar.message = res[-1] + 'Successful' if res[0] else 'Failed'
                     bar.next()
+
+        # with Bar(' Converting', max=len(files), suffix='%(percent)d%%') as bar:
+        #     for fin in files:
+        #         outfile = args.output
+        #         bar.message = fin
+        #         outfile = create_out_filepath(fin, outfile, args.type)
+        #         try:
+        #             if not (not args.override and os.path.isfile(outfile)):
+        #                 convert_file__(fin, outfile, args.classnames)
+        #                 print(' Successful')
+        #         except Exception as ex:
+        #             if not args.silent:
+        #                 raise ex
+        #             print(' Failed')
+        #         finally:
+        #             bar.next()
 
 if __name__ == "__main__":
     main__()
